a0605010adc406bab9f641c674977c21ffa7efbb
David Levine <david.a.levine@verizon.net>
2016-07-05 12:51:04 -0400

    Initial version

---
+++ neatline/exhibits/themes/waterstories-map/show.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @package     omeka
+ * @subpackage  neatline
+ * @copyright   2014 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+
+ * Copied from Neatline 7/5/16:
+ *   plugins/Neatline/views/public/exhibits/show.php
+ */
+
+?>
+
+<?php echo head(array(
+  'title' => nl_getExhibitField('title'),
+  'bodyclass' => 'neatline show'
+)); ?>
+
+<!-- Exhibit title: -->
+<h1><?php echo nl_getExhibitField('title'); ?></h1>
+
+<!-- "View Fullscreen" link: -->
+<?php echo nl_getExhibitLink(
+  null, 'fullscreen', __('View Fullscreen'), array('class' => 'nl-fullscreen')
+); ?>
+
+<!-- Exhibit and description : -->
+<?php echo nl_getExhibitMarkup(); ?>
+<?php echo nl_getNarrativeMarkup(); ?>
+
+<?php echo foot(); ?>
